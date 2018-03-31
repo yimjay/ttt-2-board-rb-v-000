69e53f95cfa547edf9d7cc7ea8f82f4879843ad8
Ji Yim <gsooyim@gmail.com>
2018-03-31 16:05:46 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,4 @@
 # Define the variable board below.
-var board = [" ", " ", " ", 
-            " ", " ", " ", 
-            " ", " ", " "]
+board = [" ", " ", " ", 
+        " ", " ", " ", 
+        " ", " ", " "]
